affe8f0032a85d1f79ca8e1e846539250e837f7c
PugzAreCute <me@pugzarecute.com>
2024-06-20 16:53:45 +0530

    [SQUASH PLZ]: pixelos bringup

--- aosp_spes.mk
+++ aosp_spes.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Additions
 $(call inherit-product-if-exists, vendor/gapps/common.mk)
